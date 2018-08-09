f39de5fef3b01efe335cad0b052e86c4d1aa1d8f
Idriss Echrif <idrissechrif@Idrisss-iMac.local>
2018-08-09 10:51:45 +0100

    fixed wrong import and changed Response to MovieResponse for clarity

--- app/src/main/java/com/example/android/popularmovies/Models/MovieResponse.java
+++ app/src/main/java/com/example/android/popularmovies/Models/MovieResponse.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class Response {
+public class MovieResponse {
     @SerializedName("page")
     private int page;
     @SerializedName("total_results")
--- app/src/main/java/com/example/android/popularmovies/Utils/ApiInterface.java
+++ app/src/main/java/com/example/android/popularmovies/Utils/ApiInterface.java
@@ -1,14 +1,15 @@
 package com.example.android.popularmovies.Utils;
 
+import com.example.android.popularmovies.Models.MovieResponse;
+
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
     @GET("movie/popular")
-    Call<Response> getPopularMovie(@Query("api_key") String apiKey);
+    Call<MovieResponse> getPopularMovies(@Query("api_key") String apiKey);
 
     @GET("movie/top_rated")
-    Call<Response> getTopRatedMovies(@Query("api_key") String apiKey);
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
 }
